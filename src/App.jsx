8aa7e9e7e68fb1bdc13d221ce46f790bd240aa53
assilbekov <askhat.assilbekov@gmail.com>
2024-04-08 10:14:46 -0300

    add box shadows

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,17 @@
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <main className="bg-slate-50 w-full h-screen"> 
+      <div>
+        <h3>Growing and animated background effect</h3>
+      </div>
+      <div>
+        <h3>Not a glowing effect, just a box shadow</h3>
+        <div className="bg-white mt-5 w-96 h-16 rounded-md shadow-sm"></div>
+        <div className="bg-white mt-5 w-96 h-16 rounded-md shadow-md"></div>
+        <div className="bg-white mt-5 w-96 h-16 rounded-md shadow"></div>
+        <div className="bg-white mt-5 w-96 h-16 rounded-md shadow-lg"></div>
+        <div className="bg-white mt-5 w-96 h-16 rounded-md shadow-xl"></div>
+      </div>
+    </main>
+  );
 }
